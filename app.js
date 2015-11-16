813f6808f8e087ea1907d59a15e45d677ab9edec
Froztic <rxcn6615@gmail.com>
2015-11-17 01:11:03 +0700

    register / login / changepass

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var secret = "EIFJHSGUY235THGL6KN7W9L0UIHQW2R0QP5QOJ9SDLFU23GSI8U6HE4TJG8PW4OP9O
 
 var models = {
 	user : require("./models/user").User,
+	patient : require("./models/user").Patient,
+	doctor : require("./models/user").Doctor,
 	medrec : require("./models/medrec").MedicalRecord,
 	appt : require("./models/appt").Appointment,
 	dept : require("./models/dept").Department,
@@ -33,12 +35,79 @@ app.get('/', function(req, res) {
 	}
 });
 
+app.get('/patient.getlist', function(req, res) {
+});
+
+app.get('/doctor.getlist', function(req, res) {
+});
+
+app.get('/*', function(req, res) {
+	res.status(404).sendFile(__dirname + '/html/404.html');
+});
+
 app.post('/user.login', jsonParser, function(req, res) {
 	var ret = {
 		success: false,
 		msg: 'undefined'
 	};
-	res.status(200).send(ret);
+	console.log('user : '+ req.body.username + ' is logging in');
+	models.user.login(req.body, function(err, msg) {
+		if(err) {
+			ret.success = false;
+			ret.msg = msg;
+			res.status(200).send(ret);
+		} else {
+			ret.success = true;
+			ret.msg = 'success';
+			ret.userObj = msg;
+			tokend.create(ret.userObj, function (error, token) {
+				if(error) {
+					console.error('failed to create token');
+				} else {
+					ret._token = token;
+				}
+				res.status(200).send(ret);
+			});
+		}
+	});
+});
+
+app.post('/user.recover', jsonParser, function(req, res) {
+	var ret = {
+		success: false,
+		msg: 'undefined'
+	};
+	models.user.recover(req.body, function(err, msg) {
+		ret.success = err? false:true;
+		ret.msg = msg;
+		res.status(200).send(ret);
+	});
+});
+
+app.post('/user.changepass', jsonParser, function(req, res) {
+	var ret = {
+		success : false,
+		msg : 'undefined'
+	};
+	logind.is_login(req.body._token, function(err, payload) {
+		if(err) {
+			ret.succss = false;
+			ret.msg = 'token error';
+			res.status(200).send(ret);
+		} else {
+			models.user.changepass( req.body, payload, function(err2, msg) {
+				if(err2) {
+					ret.success = false;
+					ret.msg = msg;
+				} else {
+					ret.success = true;
+					ret.msg = 'success';
+					console.log(msg.username + ' password changed');
+				}
+				res.status(200).send(ret)
+			});
+		}
+	});
 });
 
 app.post('/patient.register', jsonParser, function(req, res) {
@@ -69,6 +138,23 @@ tokend = (function() {
 		decode: function (token, callback) {
 			var decoded = jwt.decode(token, {complete: true});
 			callback(null, decoded,payload);
+		}
+	}
+}());
+
+logind = (function() {
+	return {
+		is_login: function (token, callback) {
+			tokend.verify(token, function(err, res) {
+				if(err) {
+					callback(err, res);
+				} else {
+					callback(null, res);
+				}
+			});
+		},
+		is_login_priv: function (token, priv, callback) {
+			
 		}
 	}
 }());
--- models/user.js
+++ models/user.js
@@ -85,6 +85,113 @@ var PharmacySchema = new mongoose.Schema({
 	}
 }, options);
 
+UserSchema.statics.login = function(data, callback) {
+	if(!data.username || !data.password) {
+		callback('input_error', 'incomplete input');
+	} else {
+		User.findOne({username : data.username, password : data.password}, {'username':1, 'priviledge':1}).lean().exec( function (err, res) {
+			if(err) {
+				callback(err, 'db error');
+			} else if(!res) {
+//				Patient.findOne({national_id : data.username, password : data.password}, {'username':1, 'priviledge':1}).lean().exec( function(err2,res2) {
+//					if(err2) {
+//						callback(err2, 'db error');
+//					} else if(!res2) {
+						callback('error', 'authen failed');
+//					} else {
+//						callback(null, res);
+//					}
+//				});
+			} else {
+				callback(null, res);
+			}
+		});
+	}
+};
+
+UserSchema.statics.recover = function(data, callback) {
+	if(!data.email) {
+		callback('input_error', 'incomplete input');
+	} else {
+		callback('error', 'not impletented');
+	}
+};
+
+UserSchema.statics.changepass = function(data, userObj, callback) {
+	if(!data.old_pass || !data.new_pass) {
+		callback('input_error', 'incomplete input');
+	} else {
+		User.findOneAndUpdate({_id: userObj._id, password : data.old_pass}, {password: data.new_pass}, {select: {'username':1}, new : true}).lean().exec( function (err, res) {
+			if(err) {
+				callback(err, 'db error');
+			} else if(!res) {
+				callback('incorrect_password', 'wrong password');
+			} else {
+				callback(null, res);
+			}
+		});
+	}
+};
+
+PatientSchema.statics.register = function(data, callback) {
+	if(!data.national_id || !data.password) {
+		callback('input_error', 'incomplete input');
+	} else {
+		Patient.findOne({national_id : data.national_id}, {'username':1}).lean().exec( function(err, res) {
+			if(err) {
+				callback(err, 'db error');
+			} else if (!res) {
+				User.findOne({email: data.email}, {'username':1}).lean().exec( function(err2, res2) {
+					if(err2) {
+						callback(err2, 'db error');
+					} else if(!res2) {
+						var new_patient = new Patient({
+							username : data.national_id,
+							password : data.password,
+							email : data.email,
+							phone : data.phone,
+							f_name : data.f_name,
+							l_name : data.l_name,
+							national_id : data.national_id,
+							birthday : data.birthday,
+							gender : data.gender,
+							address : data.address,
+							province : data.province,
+							zip_code : data.zip_code,
+							country : data.country,
+							blood_type : data.blood_type,
+							drug_hist : data.drug_hist
+//							allergy : data.allergy
+						});
+						new_patient.save( function(err3, res3) {
+							if(err3) {
+								callback(err3, 'cannot save');
+							} else {
+								callback(null, 'success');
+							}
+						});
+					} else {
+						callback('email_in_use', 'email exist');
+					}
+				});
+			} else {
+				callback('national_id_in_use', 'national_id exist');
+			}
+		});
+	}
+};
+
+PatientSchema.statics.getinfo = function(data, callback) {
+};
+
+PatientSchema.statics.getlist = function(data, callback) {
+};
+
+PatientSchema.statics.editinfo = function(data, callback) {
+};
+
+DoctorSchema.statics.getlist = function(data. callback) {
+};
 
 //pack code into model
 var User = mongoose.model('user', UserSchema, 'users');
